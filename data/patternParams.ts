ccc04505d9a031b7df6c5e458b73d15be091cac2
willindo <badshanoordeen@gmail.com>
2025-08-31 21:21:08 +0530

    fix dataparams

--- data/patternParams.ts
+++ data/patternParams.ts
@@ -1,8 +1,8 @@
 type PatternParams = {
-  linearX: Required<Pick<Params, "speed" | "amp" | "phaseGap">>;
-  waveY: Required<Pick<Params, "freq" | "amp" | "phaseGap">>;
-  circle: Required<Pick<Params, "radius" | "speed" | "phaseGap">>;
-  spiral: Required<Pick<Params, "radius" | "growth" | "speed" | "phaseGap">>;
-  pendulum: Required<Pick<Params, "angle" | "freq" | "speed">>;
-  breath: Required<Pick<Params, "amp" | "freq" | "speed" | "phaseGap">>;
+  // linearX: Required<Pick<Params, "speed" | "amp" | "phaseGap">>;
+  // waveY: Required<Pick<Params, "freq" | "amp" | "phaseGap">>;
+  // circle: Required<Pick<Params, "radius" | "speed" | "phaseGap">>;
+  // spiral: Required<Pick<Params, "radius" | "growth" | "speed" | "phaseGap">>;
+  // pendulum: Required<Pick<Params, "angle" | "freq" | "speed">>;
+  // breath: Required<Pick<Params, "amp" | "freq" | "speed" | "phaseGap">>;
 };
